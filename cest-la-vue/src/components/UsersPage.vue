97be6981714469ed2ee3885ea705830394230f22
Maksym Somushkin <syomushkin.maxim@gmail.com>
2025-03-06 21:24:01 +0100

    feat: use universal component, use data fetching

---
+++ cest-la-vue/src/components/UsersPage.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data: () => ({
+    users: null,
+  }),
+  created() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      this.users = await response.json();
+    },
+  },
+};
+</script>
+
+<template>
+  <ul v-if="!!users">
+    <li v-for="user in users" :key="`user-${user.id}`">{{ user.name }}</li>
+  </ul>
+  <p>No users</p>
+</template>
