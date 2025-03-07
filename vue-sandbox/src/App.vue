805ca0a6f1b491d3c783e0b7c2a01e4448dc4f09
Maksym Somushkin <syomushkin.maxim@gmail.com>
2025-03-07 16:46:57 +0100

    refactor(pokemons): use script setup

--- vue-sandbox/src/App.vue
+++ vue-sandbox/src/App.vue
@@ -1,22 +1,11 @@
-<script>
-// https://pokeapi.co/api/v2/pokemon?limit=151
-export default {
-  data: () => ({
-    pokedex: [1, 2, 3],
-  }),
-  methods: {
-    async fetchPokemon() {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-      this.pokedex = await response.json()
-    },
-  },
-  created() {
-    this.fetchPokemon()
-  },
-}
+<script setup>
+import Pokemons from './components/Pokemons.vue'
 </script>
 
 <template>
   <h1>New App</h1>
-  <pre>{{ pokedex }}</pre>
+  <Suspense>
+    <Pokemons />
+    <template #fallback>Loading...</template>
+  </Suspense>
 </template>
---
+++ vue-sandbox/src/components/Pokemons.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { reactive } from 'vue'
+
+const state = reactive({
+  pokedex: [1, 2, 3],
+})
+
+const fetchPokemon = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  return await response.json()
+}
+
+state.pokedex = await fetchPokemon()
+</script>
+
+<template>
+  <pre>{{ state.pokedex }}</pre>
+</template>
